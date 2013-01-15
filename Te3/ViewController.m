2f653dee93507d9f86dfc281b39329e9658254d7
veveer <veveer@126.com>
2013-01-15 09:16:41 +0800

    Test
    This is for test.

--- Te3/ViewController.m
+++ Te3/ViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "ViewController.h"
-
+#import <SDWebImage/UIImageView+WebCache.h>
 @interface ViewController ()
 
 @end
@@ -17,7 +17,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    SDWebImageManager *manager = [SDWebImageManager sharedManager]; 
 }
 
 - (void)viewDidUnload
