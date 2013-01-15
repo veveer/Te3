64945c626fe518fd5b07faedb0d47518fdc3056d
gyl <zzf_jinan@svnserve.(none)>
2013-01-15 08:52:06 +0800

    Initial Commit

---
+++ Te3/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Te3
+//
+//  Created by zf z on 13-1-15.
+//  Copyright (c) 2013年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Te3/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Te3
+//
+//  Created by zf z on 13-1-15.
+//  Copyright (c) 2013年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
